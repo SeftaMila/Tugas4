bc59bc4d864aa8c36ac14f6bea0b63b41eccc199
SeftaMila <98940087+SeftaMila@users.noreply.github.com>
2022-05-01 07:26:15 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,135 @@
+let vs = []
+function setup (){
+  createCanvas( 400,400);
+  v =new Vehicle (200,200);
+}
+
+function draw (){
+   background(242);
+  
+  v.display()
+  v.edges()
+  v.update();
+  v.wander();
+
+}
+
+class Vehicle{
+  constructor(x,y){
+    this.location = createVector(x,y);
+    this.velocity = createVector(1,0);
+    this.acceleration = createVector(0,0);
+    this.l = 30;
+    this.maxspeed = 1;
+    this.maxforce = 0.01;
+    this.wanderTheta = PI/2;
+  }
+  
+  wander(){
+    let projVector = this.velocity.copy();
+    projVector.setMag(100);
+    let projPoint = projVector.add(this.location);
+    
+    let wanderRadius = 50;
+    let theta = this.wanderTheta + this.velocity.heading();
+    let xBar = wanderRadius * cos(theta);
+    let yBar = wanderRadius * sin(theta);
+    
+    let wanderPoint = p5.Vector.add(projPoint, createVector(xBar, yBar));
+    
+    let debug = true;
+    
+    if (debug){
+      push()
+      fill(150, 75, 0);
+      stroke('green')
+      line(this.location.x, this.location.y, projPoint.x, projPoint.y)
+      stroke('red');
+      circle(projPoint.x, projPoint.y, 8);
+      noFill();
+      stroke('red');
+      circle(projPoint.x, projPoint.y, wanderRadius*2);
+      
+      line(this.location.x, this.location.y, wanderPoint.x, wanderPoint.y)
+      stroke('black')
+      fill('black')
+      rect(wanderPoint.x, wanderPoint.y, 20);
+      
+      fill('yellow')
+      ellipse(wanderPoint.x, wanderPoint.y, -16);
+      pop()
+    }
+    
+    let steeringForce = wanderPoint.sub(this.location);
+    steeringForce.setMag(this.maxforce);
+    this.applyForce(steeringForce);
+    
+    this.wanderTheta += random(-0.5, 0.5);
+  }
+  
+  seek(vektorTarget){
+    // percieve target location 
+    var desired = p5.Vector.sub(vektorTarget, this.location);
+    desired.normalize();
+    desired.mult(this.maxspeed);
+    
+    //kemudi
+    var steer = p5.Vector.sub(desired, this.velocity);
+    steer.limit(this.maxforce);
+    this.applyForce(steer);
+  }
+  
+  arrive(vektorTarget){
+    // percieve target location
+    var desired = p5.Vector.sub(vektorTarget, this.location);
+    var jarak = desired.mag()
+
+    if (jarak < 100){
+      var m = map(jarak, 0, 100, 0, this.maxspeed);
+      desired.normalize();
+      desired.mult(m);
+    }
+    else{
+      desired.normalize();
+      desired.mult(this.maxspeed);    
+    }
+    
+    //kemudi
+    var steer = p5.Vector.sub(desired, this.velocity);
+    steer.limit(this.maxforce);
+    this.applyForce(steer);
+  }
+  
+  update(){
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxspeed);
+    this.location.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+  applyForce(force){
+    this.acceleration.add(force);
+  }
+  display(){
+    var theta = this.velocity.heading()// + PI/2;
+    push();
+    noStroke();
+    translate(this.location.x, this.location.y)
+    rotate(theta)
+    fill('blue')
+     triangle(0, this.l/2, 0, -this.l/2, this.l,0)
+    pop();
+  }
+
+  edges() {
+    if (this.location.x > width + 10) {
+      this.location.x = -10;
+    } else if (this.location.x < -10) {
+      this.location.x = width + 10;
+    }
+    if (this.location.y > height + 10) {
+      this.location.y = -10;
+    } else if (this.location.y < -10) {
+      this.location.y = height + 10;
+    }
+  }
+}
